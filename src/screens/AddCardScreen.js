31884adae6f62972083e4e8f30e66aea13c8f037
arslan-khan <muhammad.arslan@arbisoft.com>
2018-10-14 14:04:18 +0500

    quiz screen with UI plus functionality

--- src/screens/AddCardScreen.js
+++ src/screens/AddCardScreen.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
 import {
-  Content, Card, Item, Input, Button, Text, Toast,
+  Content, Card, Item, Input, Button, Text, Toast, Icon,
 } from 'native-base';
 import { StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { NavigationActions } from 'react-navigation';
 
 import { addNewCardRequest } from '../actions/decksActions';
+import { GREY } from '../constants/colors';
 
 class AddCardScreen extends Component {
   static propTypes = {
     addNewCardRequest: PropTypes.func.isRequired,
     deckId: PropTypes.string.isRequired,
+    navigation: PropTypes.shape({}).isRequired,
   };
 
-  state = { question: '', answer: '' };
+  state = {
+    question: '',
+    answer: '',
+    yesAnswer: false,
+    noAnswer: false,
+  };
 
   questionInputHandler = question => this.setState(() => ({ question }));
 
-  answerInputHandler = answer => this.setState(() => ({ answer }));
+  yesClickHandler = () => this.setState(() => ({ yesAnswer: true, noAnswer: false, answer: 'yes' }));
+
+  noClickHandler = () => this.setState(() => ({ yesAnswer: false, noAnswer: true, answer: 'no' }));
 
   onPressHandler = () => {
-    const { deckId, addNewCardRequest } = this.props;
+    const { deckId, addNewCardRequest, navigation } = this.props;
 
     this.setState(() => ({ question: '', answer: '' }));
     addNewCardRequest(this.state, deckId);
@@ -30,11 +40,22 @@ class AddCardScreen extends Component {
       text: 'You have successfully added a New Card.',
       duration: 3000,
     });
+
+    navigation.dispatch(
+      NavigationActions.navigate({
+        routeName: 'Home',
+        action: NavigationActions.back({ routeName: 'Home' }),
+      }),
+    );
   };
 
   render() {
-    const { question, answer } = this.state;
-    const { card, inputField } = styles;
+    const {
+      question, answer, yesAnswer, noAnswer,
+    } = this.state;
+    const {
+      card, inputField, answerText, yesBtn, noBtn,
+    } = styles;
 
     return (
       <Content padder>
@@ -49,14 +70,33 @@ class AddCardScreen extends Component {
             />
           </Item>
 
-          <Item rounded style={inputField}>
-            <Input
-              placeholder="Answer here..."
-              value={answer}
-              onChangeText={this.answerInputHandler}
-              autoCorrect={false}
-            />
-          </Item>
+          <Text style={answerText}>Please tap on Yes or No to choose your answer.</Text>
+
+          <Button
+            style={yesBtn}
+            iconLeft={yesAnswer}
+            block
+            success
+            rounded
+            bordered
+            onPress={this.yesClickHandler}
+          >
+            {yesAnswer && <Icon name="checkmark" />}
+            <Text>Yes</Text>
+          </Button>
+
+          <Button
+            style={noBtn}
+            iconLeft={noAnswer}
+            block
+            danger
+            rounded
+            bordered
+            onPress={this.noClickHandler}
+          >
+            {noAnswer && <Icon name="checkmark" />}
+            <Text>No</Text>
+          </Button>
 
           <Button
             block
@@ -76,6 +116,9 @@ class AddCardScreen extends Component {
 const styles = StyleSheet.create({
   card: { padding: 20 },
   inputField: { marginBottom: 10 },
+  answerText: { textAlign: 'center', color: GREY },
+  yesBtn: { marginTop: 20, marginBottom: 5 },
+  noBtn: { marginTop: 5, marginBottom: 10 },
 });
 
 const mapStateToProps = (state, { navigation }) => ({
--- src/screens/QuizScreen.js
+++ src/screens/QuizScreen.js
@@ -1,16 +1,147 @@
 import React, { Component } from 'react';
-import { View, Text, H1 } from 'native-base';
+import {
+  Text, Spinner, Card, Button, Content, H1,
+} from 'native-base';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { StyleSheet } from 'react-native';
+import FlipCard from 'react-native-flip-card';
+
+import { fetchDeckRequest } from '../actions/decksActions';
+import { TOMATO, GREY } from '../constants/colors';
 
 class QuizScreen extends Component {
+  static defaultProps = { selectedDeck: null };
+
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    selectedDeck: PropTypes.shape({}),
+    deckId: PropTypes.string.isRequired,
+    fetchDeckRequest: PropTypes.func.isRequired,
+    navigation: PropTypes.shape({}).isRequired,
+  };
+
+  state = {
+    currentQuestionNumber: 0,
+    correctAnswers: 0,
+    InCorrectAnswers: 0,
+    cardFlipped: false,
+  };
+
+  componentDidMount = () => {
+    const { deckId, fetchDeckRequest } = this.props;
+    fetchDeckRequest(deckId);
+  };
+
+  updateQuestionNumber = () => this.setState(({ currentQuestionNumber }) => ({
+    currentQuestionNumber: currentQuestionNumber + 1,
+  }));
+
+  flipCard = () => this.setState(({ cardFlipped }) => ({ cardFlipped: !cardFlipped }));
+
+  correctClickHandler = () => {
+    this.updateQuestionNumber();
+    this.flipCard();
+    this.setState(({ correctAnswers }) => ({ correctAnswers: correctAnswers + 1 }));
+  };
+
+  inCorrectClickHandler = () => {
+    this.updateQuestionNumber();
+    this.flipCard();
+    this.setState(({ InCorrectAnswers }) => ({ InCorrectAnswers: InCorrectAnswers + 1 }));
+  };
+
   render() {
+    const { loading, selectedDeck } = this.props;
+    const {
+      currentQuestionNumber, cardFlipped, correctAnswers, InCorrectAnswers,
+    } = this.state;
+    const {
+      main, secondary, button, count, correctBtn,
+    } = styles;
+
+    if (loading || !selectedDeck) return <Spinner color={TOMATO} />;
+
+    const { title, questions } = selectedDeck;
+    const currentQuestion = questions[currentQuestionNumber];
+
+    if (currentQuestionNumber === questions.length) {
+      return (
+        <Content padder>
+          <Card>
+            <Text style={secondary}>{`You got ${correctAnswers} questions correct.`}</Text>
+            <Text style={secondary}>{`You got ${InCorrectAnswers} questions incorrect.`}</Text>
+            <Text style={secondary}>
+              {`That is a score of ${((correctAnswers / questions.length) * 100).toFixed(2)}%.`}
+            </Text>
+          </Card>
+        </Content>
+      );
+    }
+
     return (
-      <View>
-        <Text>
-          <H1>Quiz Screen</H1>
-        </Text>
-      </View>
+      <Content padder>
+        <FlipCard
+          style={{ borderWidth: 0, flex: 1 }}
+          friction={6}
+          perspective={1000}
+          flipHorizontal
+          flipVertical={false}
+          clickable={false}
+          flip={cardFlipped}
+        >
+          {/* Front Side */}
+          <Card>
+            <Text style={main}>
+              <H1>{title}</H1>
+            </Text>
+            <Text style={secondary}>{currentQuestion.question}</Text>
+
+            <Button rounded style={button} primary onPress={this.flipCard}>
+              <Text>Show Answer</Text>
+            </Button>
+
+            <Text style={count}>{`${currentQuestionNumber + 1} | ${questions.length}`}</Text>
+          </Card>
+
+          {/* Back Side */}
+          <Card>
+            <Text style={main}>
+              <H1>{currentQuestion.question}</H1>
+            </Text>
+            <Text style={secondary}>{currentQuestion.answer}</Text>
+
+            <Button rounded style={[button, correctBtn]} primary onPress={this.correctClickHandler}>
+              <Text>Correct</Text>
+            </Button>
+            <Button rounded style={button} danger onPress={this.inCorrectClickHandler}>
+              <Text>InCorrect</Text>
+            </Button>
+          </Card>
+        </FlipCard>
+      </Content>
     );
   }
 }
 
-export default QuizScreen;
+const styles = StyleSheet.create({
+  main: { paddingTop: 40, textAlign: 'center', marginBottom: 10 },
+  secondary: { textAlign: 'center', marginBottom: 40, color: GREY },
+  button: {
+    marginBottom: 20,
+    alignSelf: 'center',
+  },
+  correctBtn: { marginBottom: 10 },
+  count: { paddingLeft: 20, color: GREY, marginBottom: 20 },
+});
+
+const mapStateToProps = ({ decks }, { navigation }) => ({
+  loading: decks.loading,
+  selectedDeck: decks.selectedDeck,
+  deckId: navigation.state.params.deckId,
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchDeckRequest },
+)(QuizScreen);
